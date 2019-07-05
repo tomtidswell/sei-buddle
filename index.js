f2fd1da8371ae81e189171170fb140ac10e6734a
tomtidswell <tomtidswell@gmail.com>
2019-07-05 10:06:27 +0100

    project setup complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+require('dotenv').config()
+
+app.use(bodyParser.json())
+
+console.log('this is the back end!!! Hello world')
+
+app.listen(process.env.PORT, () => console.log(`App is listening on port ${process.env.PORT}`))
