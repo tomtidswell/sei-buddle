9fbfa07ebe0091ad524eae49107cad361a70a0ea
simbrar1 <sim.brar1@hotmail.com>
2019-07-08 17:33:15 +0100

    logins

---
+++ src/components/auth/Register.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import axios from 'axios'
+
+class Register extends React.Component {
+  constructor() {
+    super()
+
+
+    this.state = { data: {} }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    this.setState({ data })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post('/api/register/', this.state.data)
+      .then(() => this.props.history.push('/login'))
+      .catch(err => console.log(err.response))
+  }
+
+  render() {
+    return (
+      <main>
+        <section className="container">
+          <form onSubmit={this.handleSubmit}>
+            <h2 className="title">Register</h2>
+            <div className="field">
+              <label className="label">Username</label>
+              <div className="control">
+                <input
+                  className="input"
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password Confirmation</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  name="passwordConfirmation"
+                  placeholder="Password Confirmation"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <button type="submit" className="button">Submit</button>
+          </form>
+        </section>
+      </main>
+    )
+  }
+}
+
+export default Register
