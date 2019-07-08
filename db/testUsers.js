71529dcf80f90475229614d33e6cc3ba4b14b6b4
tomtidswell <tomtidswell@gmail.com>
2019-07-08 13:21:55 +0100

    Create testing is working

---
+++ db/testUsers.js
@@ -0,0 +1,11 @@
+module.exports = [{
+  username: 'Testuser One',
+  email: 'testuser1@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+},{
+  username: 'Testuser Two',
+  email: 'testuser2@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}]
--- test/controllers/events_test.js
+++ test/controllers/events_test.js
@@ -2,20 +2,32 @@
 
 require('../spec_helper')
 const Events = require('../../models/event')
+const User = require('../../models/user')
+const eventsData = require('../../db/seedsDataEvents')
+const userData = require('../../db/seedsDataUsers')
 
+let userToken = null
+let userCounter = 0
 
-// a test for the Events controller
+// a test for the Events controller, which includes the registration and login of a single user to test authentication
 describe('Event Controller Test', ()=>{
 
-  // empty the database of all Events before running each test
+  // empty the database of all Events and Users before running each test
   beforeEach(done => {
     Events.collection.deleteMany()
-    done()
+    //User.collection.deleteMany()
+
+    User.create(userData[userCounter])
+      .then(() => done())
+      .catch(() => done())
+
+    userCounter++
   })
 
-  // empty the database of all Events after running each test
+  // empty the database of all Events and Users after running each test
   afterEach(done => {
     Events.collection.deleteMany()
+    //User.collection.deleteMany()
     done()
   })
 
@@ -24,19 +36,7 @@ describe('Event Controller Test', ()=>{
 
     //before each sub-test, create an entry in the database
     beforeEach(done => {
-      Events.create([{
-        category: 'Sports',
-        subcategory: 'Tennis',
-        name: 'Tennis at Saturday',
-        description: 'I am looking for someone to play tennis with me on Saturday 13th July. I have booked a court in Hyde Park for 3pm, please RSVP below.',
-        location: {
-          line1: 'Hyde Park London',
-          city: 'London',
-          postcode: 'W2 2UH'
-        },
-        postcode: 'W2 2UH',
-        date: '01/01/2019'
-      }])
+      Events.create(eventsData[0])
         .then(() => done())
         .catch(() => done())
     })
@@ -140,37 +140,11 @@ describe('Event Controller Test', ()=>{
         })
     })
 
+
     describe('Make more than one Events', () => {
 
       beforeEach(done => {
-        Events.create([
-          {
-            category: 'Sports',
-            subcategory: 'Tennis',
-            name: 'Tennis at Saturday',
-            description: 'I am looking for someone to play tennis with me on Saturday 13th July. I have booked a court in Hyde Park for 3pm, please RSVP below.',
-            location: {
-              line1: 'Hyde Park London',
-              city: 'London',
-              postcode: 'W2 2UH'
-            },
-            postcode: 'W2 2UH',
-            date: '01/01/2019'
-          },
-          {
-            category: 'Sports',
-            subcategory: 'Tennis',
-            name: 'Tennis at Saturday',
-            description: 'I am looking for someone to play tennis with me on Saturday 13th July. I have booked a court in Hyde Park for 3pm, please RSVP below.',
-            location: {
-              line1: 'Hyde Park London',
-              city: 'London',
-              postcode: 'W2 2UH'
-            },
-            postcode: 'W2 2UH',
-            date: '01/01/2019'
-          }
-        ])
+        Events.create([{ ...eventsData[3] }, { ...eventsData[4] }])
           .then(() => done())
           .catch(done)
       })
@@ -180,77 +154,22 @@ describe('Event Controller Test', ()=>{
           .get('/api/events')
           .set('Accept', 'application/json')
           .end((err, res) => {
+            //console.log(res.body)
             expect(res.body.length).to.equal(3)
             done()
           })
       })
     })
-
   })
-
-
-  describe('POST /api/events', () => {
-
-    it('should return a 201 response', done => {
-      api
-        .post('/api/events')
-        .set('Accept', 'application/json')
-        .send({
-
-        })
-        .expect(201, done)
-    })
-
-    it('should create a Events', done => {
-      api
-        .post('/api/events')
-        .set('Accept', 'application/json')
-        .send({
-
-        })
-        .end((err, res) => {
-          const events = res.body
-
-          expect(events)
-            .to.have.property('_id')
-            .and.to.be.a('string')
-
-          expect(events)
-            .to.have.property('brand')
-            .and.to.be.a('string')
-
-          expect(events)
-            .to.have.property('colour')
-            .and.to.be.a('string')
-
-          expect(events)
-            .to.have.property('price')
-            .and.to.be.a('number')
-
-          expect(events)
-            .to.have.property('createdAt')
-            .and.to.be.a('string')
-
-          expect(events)
-            .to.have.property('updatedAt')
-            .and.to.be.a('string')
-
-          done()
-        })
-    })
-  })
-
 
   describe('GET /api/events/:id', () => {
 
-    let events
+    let newEvent = null
 
     beforeEach(done => {
-      events.create({
-
-      })
-        .then(eventsData => {
-          events = eventsData
+      Events.create(eventsData[0])
+        .then(theEvent => {
+          newEvent = theEvent
           done()
         })
         .catch(done)
@@ -258,31 +177,168 @@ describe('Event Controller Test', ()=>{
 
     it('should return a 200 response', done => {
       api
-        .get(`/api/events/${events.id}`)
+        .get(`/api/events/${newEvent.id}`)
         .set('Accept', 'application/json')
         .expect(200, done)
     })
   })
 
-  describe('DELETE /api/events/:id', () => {
 
-    let events
+  // describe('CREATE a USER by POST to /api/register', () => {
+  //
+  //   it('should create a user account', done => {
+  //     api
+  //       .post('/api/register')
+  //       .set('Accept', 'application/json')
+  //       .send(userData[userCounter])
+  //       .end((err, res) => {
+  //         userCounter++
+  //         const registered = res.body
+  //         //registeredUser = res.body.user
+  //         //console.log(registered)
+  //
+  //         expect(registered)
+  //           .to.have.property('user')
+  //           .and.to.be.an('object')
+  //
+  //         expect(registered)
+  //           .to.have.property('message')
+  //           .and.to.be.a('string')
+  //
+  //         done()
+  //       })
+  //   })
+  // })
 
-    beforeEach(done => {
-      events.create({
 
-      })
-        .then(eventsData => {
-          events = eventsData
+  describe('LOGIN the template USER by POST to /api/login', () => {
+
+    it('the user should be able to log in', done => {
+      const request = { email: userData[0].email, password: userData[0].password }
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send(request)
+        .end((err, res) => {
+          //console.log(res.body)
+          userToken = res.body.token
           done()
         })
-        .catch(done)
+    })
+  })
+
+
+  describe('POST /api/events', () => {
+
+    it('the user should be able to log in', done => {
+      const request = { email: userData[0].email, password: userData[0].password }
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send(request)
+        .end((err, res) => {
+          //console.log(res.body)
+          userToken = res.body.token
+          done()
+        })
     })
 
-    it('should return a 204 response', done => {
+    it('should return a 201 response', done => {
       api
-        .delete(`/api/events/${events.id}`)
+        .post('/api/events')
         .set('Accept', 'application/json')
+        .set('Authorization', userToken)
+        .send(eventsData[5])
+        .expect(201, done)
+    })
+
+    it('should create an Event', done => {
+      api
+        .post('/api/events')
+        .set('Accept', 'application/json')
+        .set('Authorization', userToken)
+        .send(eventsData[6])
+        .end((err, res) => {
+          const oneEvent = res.body
+          //console.log(res.body)
+          expect(oneEvent)
+            .to.have.property('_id')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('createdAt')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('updatedAt')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('attendees')
+            .and.to.be.an('array')
+          expect(oneEvent)
+            .to.have.property('category')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('comments')
+            .and.to.be.an('array')
+          expect(oneEvent)
+            .to.have.property('date')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('description')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('location')
+            .and.to.be.an('array')
+          expect(oneEvent)
+            .to.have.property('name')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('postcode')
+            .and.to.be.a('string')
+          expect(oneEvent)
+            .to.have.property('subcategory')
+            .and.to.be.a('string')
+          done()
+        })
+    })
+  })
+
+
+  describe('DELETE /api/events/:id', () => {
+
+    let newEvent = null
+
+
+    beforeEach(done => {
+      const request = { email: userData[0].email, password: userData[0].password }
+
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send(request)
+        .end((err, res) => {
+          console.log(res.body)
+          userToken = res.body.token
+        })
+
+      api
+        .post('/api/events')
+        .set('Accept', 'application/json')
+        .set('Authorization', userToken)
+        .send(eventsData[5])
+        .end((err, res) => {
+          console.log('response after creating',res.body)
+          newEvent = res.body._id
+          done()
+        })
+    })
+
+
+    it('should return a 204 response when deleting', done => {
+      console.log('New event id:', newEvent)
+      api
+        .delete(`/api/events/${newEvent}`)
+        .set('Accept', 'application/json')
+        .set('Authorization', userToken)
         .expect(204, done)
     })
   })
