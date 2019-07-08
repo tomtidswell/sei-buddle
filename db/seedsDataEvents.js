22cfde095ab65a299fc91c5a4b8c0a1e1ec8784f
simbrar1 <sim.brar1@hotmail.com>
2019-07-08 09:33:17 +0100

    seeds done

--- db/seedsDataEvents.js
+++ db/seedsDataEvents.js
@@ -9,4 +9,232 @@ module.exports = [{
     postcode: 'W2 2UH'
   },
   postcode: 'W2 2UH'
+}, {
+
+  category: 'Sports',
+  subcategory: 'Tennis',
+  name: 'Buddy up for Tennis on Wednesday',
+  description: 'I would like to play tennis on Wednesday the 17th - please let me know if you are interested.',
+  location: {
+    line1: 'Wembley',
+    city: 'London',
+    postcode: 'W1 5KJ'
+  },
+  postcode: 'W1 5KJ'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Football',
+  name: 'Football 5 a side',
+  description: 'I have booked a pitch for next Sunday - looking for anyone who wants to play a 5 a side game.',
+  location: {
+    line1: 'Shoreditch',
+    city: 'London',
+    postcode: 'E1 6JE'
+  },
+  postcode: 'E1 6JE'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Football',
+  name: 'A causal kick about',
+  description: 'If anyone is free for a kick about this Friday let me know and we can go to Greenwich park',
+  location: {
+    line1: 'Greenwich',
+    city: 'London',
+    postcode: 'SE10 1BU'
+  },
+  postcode: 'SE10 1BU'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Hockey',
+  name: 'Booked Astro for 14th July',
+  description: 'Looking for a anyone who is interested in some Hockey practise',
+  location: {
+    line1: 'Tower Hamlets',
+    city: 'London',
+    postcode: 'E1W 2QD'
+  },
+  postcode: 'E1W 2QD'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Hockey',
+  name: 'Booked Astro for 27th July',
+  description: 'FREE Astro truf booked, let me know if you are interested',
+  location: {
+    line1: 'Clapham',
+    city: 'London',
+    postcode: 'SW12 8EN'
+  },
+  postcode: 'SW12 8EN'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Basketball',
+  name: 'Basketball court booked, need friends',
+  description: 'I have a court booked for the 18th July, limited spaces left so RSVP',
+  location: {
+    line1: 'Islington',
+    city: 'London',
+    postcode: 'EC1V 3PU'
+  },
+  postcode: 'EC1V 3PU'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Basketball',
+  name: 'Bball game 6 a side',
+  description: '6 a side Basketball game, located in Oval - easy to get to. RSVP now to play.',
+  location: {
+    line1: 'Oval',
+    city: 'London',
+    postcode: 'SE5 0UB'
+  },
+  postcode: 'SE5 0UB'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Ping Pong',
+  name: 'Just wanting to play a game of pong',
+  description: 'I have just moved to London and would like to play Ping Pong, if anyone would like to play with me please let me know.',
+  location: {
+    line1: 'Farringdon',
+    city: 'London',
+    postcode: 'EC1A 2AL'
+  },
+  postcode: 'EC1A 2AL'
+
+}, {
+  category: 'Sports',
+  subcategory: 'Netball',
+  name: 'Netball buddy required - County Standard ONLY',
+  description: 'Need a GK/GD to help me practise shooting, let me know',
+  location: {
+    line1: 'Islington',
+    city: 'London',
+    postcode: 'EC1V 3PU'
+  },
+  postcode: 'EC1V 3PU'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Languages',
+  name: 'Wanting to learn some Spanish',
+  description: 'I am setting off to Barcelona for a very long travel, would be greatful for a tutor in Spanish. In return I know some Italian, so we can trade.',
+  location: {
+    line1: 'My house - Balham',
+    city: 'London',
+    postcode: 'SW12 9AL'
+  },
+  postcode: 'SW12 9AL'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Languages',
+  name: 'French',
+  description: 'I would like to learn French, if anyone can help let me know. In return I can play the piano.',
+  location: {
+    line1: 'Shadwell',
+    city: 'London',
+    postcode: 'E1 2QJ'
+  },
+  postcode: 'E1 2QJ'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Music',
+  name: 'Trumpet',
+  description: 'I am offering a free lesson to anyone who is interested in playing the Trumpet',
+  location: {
+    line1: 'Notting',
+    city: 'London',
+    postcode: 'W11'
+  },
+  postcode: 'W11'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Music',
+  name: 'Drum and Guitar jam sesh',
+  description: 'I can play the drums, if you can play guitar lets have a jam sesh.',
+  location: {
+    line1: 'Claham South',
+    city: 'London',
+    postcode: 'SW12 9DU'
+  },
+  postcode: 'SW12 9DU'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Art',
+  name: 'I would like to learn how to paint - please help!',
+  description: 'If anyone has any free time, I would appreciate an Art lesson, in return I can help with any small labour jobs.',
+  location: {
+    line1: 'Pimlico',
+    city: 'London',
+    postcode: 'SW1V1AJ'
+  },
+  postcode: 'SW1V1AJ'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Art',
+  name: 'Free spray paint lesson',
+  description: 'Free spray paint lesson on stencils',
+  location: {
+    line1: 'Edgware',
+    city: 'London',
+    postcode: 'W2 1DH'
+  },
+  postcode: 'W2 1DH'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Food',
+  name: 'URGENT chef required',
+  description: 'I told the girl I am seeing I used to be chef and now she is coming to sample my cooking. I need someone to make a gourmet dinner ASAP, willing to pay the price.',
+  location: {
+    line1: 'Brixton',
+    city: 'London',
+    postcode: 'SW2 1QN'
+  },
+  postcode: 'SW2 1QN'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Food',
+  name: 'Free cooking class',
+  description: 'I have a catering company but have decided to take the weeknd off for anyone who would be interested in a free cooking class. RSVP.',
+  location: {
+    line1: 'Madia Vale',
+    city: 'London',
+    postcode: 'W9 1AE'
+  },
+  postcode: 'W9 1AE'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Dance',
+  name: 'Moonwalking lesson',
+  description: 'Some would say I am the next best thing when it comes to Moonwalking, RSVP to the event and learn the Moonwalk.',
+  location: {
+    line1: 'Aldgate',
+    city: 'London',
+    postcode: 'EC3N 1AH'
+  },
+  postcode: 'EC3N 1AH'
+
+}, {
+  category: 'Skills',
+  subcategory: 'Lock Picking',
+  name: 'Ever wondered how to pick a lock?',
+  description: 'Free tutorial on how to pick a lock....it will come in handy if you ever get kidnapped or have simply lost your key. A great skill to have, in a sticky situation.',
+  location: {
+    line1: 'Tooting',
+    city: 'London',
+    postcode: 'SW17 0TF'
+  },
+  postcode: 'SW17 0TF'
 }]
--- db/seedsDataUsers.js
+++ db/seedsDataUsers.js
@@ -1,6 +1,111 @@
 module.exports = [{
   username: 'Char Lodge',
-  email: 'char@emai',
+  email: 'char@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Kathy Gee',
+  email: 'Kg@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Hildy Pipe',
+  email: 'Hildy1@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Hildy Pipe',
+  email: 'Hildy1@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'John Page',
+  email: 'Johnny1@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Sam Watts',
+  email: 'Watts@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Jeremy Flatt',
+  email: 'JFlat@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Karen Pritch',
+  email: 'Karen@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Danny Je',
+  email: 'Danny@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Poe Strong',
+  email: 'Poe@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+
+}, {
+  username: 'Rennie Shraw',
+  email: 'Ren@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Krin Polter',
+  email: 'Krin@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Devlin Wills',
+  email: 'Dev@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Shuh Tsui',
+  email: 'Shuht@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Flin Wing',
+  email: 'Flin@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Den Brow',
+  email: 'Den@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Lucas VonTrout',
+  email: 'LVT@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Grace Nots',
+  email: 'Grace@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Philly Mac',
+  email: 'Philly@email',
+  password: 'pass',
+  passwordConfirmation: 'pass'
+}, {
+  username: 'Lars Sou',
+  email: 'Lars@email',
   password: 'pass',
   passwordConfirmation: 'pass'
 }]
