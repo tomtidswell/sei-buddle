fc627779c3976a2548a9fc0349450a31b7d3f335
Sheema <sa_khan94@hotmail.co.uk>
2019-07-09 17:58:13 +0100

    navbar profile view

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,17 +1,49 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+import Auth from '../../lib/Auth'
 
-const Navbar = () => {
-  return (
-    <nav className="top-navigation">
-      <Link to='/'>Home</Link>
-      <Link to='/events'>Index</Link>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/events/new'>Create New Event</Link>
 
-    </nav>
-  )
+class Navbar extends React.Component {
+  constructor() {
+    super()
+
+    this.state = { }
+  }
+
+  componentDidMount() {
+    this.getUserId()
+    this.setLocationInState()
+  }
+
+  getUserId() {
+    this.setState({ userId: Auth.getPayload().sub })
+  }
+
+  componentDidUpdate() {
+    if (this.state.previousLocation !== this.props.history.location) {
+      this.getUserId()
+      this.setLocationInState()
+    }
+  }
+
+  setLocationInState() {
+    this.setState({ previousLocation: this.props.history.location })
+  }
+
+  render() {
+    if (!this.state.userId) return null
+    return (
+      <nav className="top-navigation">
+        <Link to='/'>Home</Link>
+        <Link to='/events'>Index</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/events/new'>Create New Event</Link>
+        <Link to={`/users/${this.state.userId}`}>View Profile</Link>
+
+      </nav>
+    )
+  }
 }
 
-export default Navbar
+export default withRouter(Navbar)
