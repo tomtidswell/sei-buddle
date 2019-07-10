ed849870a06f7a5e6e444c5d27cf4653f5466f74
tomtidswell <tomtidswell@gmail.com>
2019-07-10 10:53:54 +0100

    Style bugfix

--- src/components/events/EventsCard.js
+++ src/components/events/EventsCard.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
-const EventsCard = ({ name, postcode, price, date, _id, user, attendees, totalAttendees }) => {
+const EventsCard = ({ name, postcode, price, date, _id, user, attendees, totalAttendees, description }) => {
   return (
     <div className="event-card">
 
-      <Link to={`/events/${_id}`}>
-        <div className="card-header">
+      <div className="card-header">
+        <Link to={`/events/${_id}`}>
           <h4 className="card-header-title">{name}</h4>
           <h4 className="card-header-title">{postcode}</h4>
-        </div>
-      </Link>
+        </Link>
+      </div>
 
       <div className="card-content">
         <div className="event-users">
